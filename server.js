2a936a3e87b35d40b5bb2b4cb13043b7dd07f1e2
GANG LIU <liu.ga@husky.neu.edu>
2014-10-09 12:18:00 -0700

    UPDATE A REMOTE DB

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var mongojs = require('mongojs');
-var db = mongojs("test", ["serviceClients"]);
-
+// var db = mongojs("test", ["serviceClients"]);
 var app = express();
-//var applications = require('./public/features/services/server.js');
+var applications = require('./public/features/services/server.js');
 
-// app.use(express.static(__dirname + '/public'));
-// app.use(express.bodyParser());
+var mongodbConnectionString = process.env.OPENSHIFT_MONGODB_DB_URL + "atest";
+
+if(typeof process.env.OPENSHIFT_MONGODB_DB_URL == "undefined"){
+	mongodbConnectionString = "atest";
+}
+
+var db =mongojs(mongodbConnectionString, ["serviceClients"]);
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(express.bodyParser());
 
 
 //  Set the environment variables we need.
@@ -17,49 +27,49 @@ app.get("/env", function (req, res){
 	res.json(process.env);
 });
 // //listen quest or url
-// app.get("/serviceClients", function(req, res){
+app.get("/serviceClients", function(req, res){
 
-//     db.serviceClients.find(function(err, docs){
-//     	res.json(docs);
-//     });
-// });
+    db.serviceClients.find(function(err, docs){
+    	res.json(docs);
+    });
+});
 
-// app.post("/serviceClients", function(req, res){
-// 	var svc = req.body;
-// 	db.serviceClients.insert(req.body, function(err, doc){
-// 		res.json(doc);
-// 	});
-// });
+app.post("/serviceClients", function(req, res){
+	var svc = req.body;
+	db.serviceClients.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
+});
 
-// app.get("/serviceClients/:id", function(req, res){
-// 	var id = req.params.id;
-// 	db.serviceClients.findOne({_id : mongojs.ObjectId(id)}, function(err, doc){
-// 		res.json(doc);
-// 	});
-// });
-
-
-// app.put("/serviceClients/:id", function (req, res) {
-//     db.serviceClients.findAndModify({
-//         // find the object by id
-//         query: { _id: mongojs.ObjectId(req.params.id) },
-//         // new values are in req.body, update it's name
-//         update: { $set: { name: req.body.name } },
-//         // single one
-//         new: true
-//     }, function (err, doc, lastErrorObject) {
-//         // respond with the new document
-//         res.json(doc);
-//     });
-// });
+app.get("/serviceClients/:id", function(req, res){
+	var id = req.params.id;
+	db.serviceClients.findOne({_id : mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+});
 
 
-// app.delete("/serviceClients/:id", function(req, res){
-// 	var id = req.params.id;
-// 	db.serviceClients.remove({_id : mongojs.ObjectId(id)}, function(err, doc){
-// 		res.json(doc);
-// 	});
+app.put("/serviceClients/:id", function (req, res) {
+    db.serviceClients.findAndModify({
+        // find the object by id
+        query: { _id: mongojs.ObjectId(req.params.id) },
+        // new values are in req.body, update it's name
+        update: { $set: { name: req.body.name } },
+        // single one
+        new: true
+    }, function (err, doc, lastErrorObject) {
+        // respond with the new document
+        res.json(doc);
+    });
+});
 
-// });
+
+app.delete("/serviceClients/:id", function(req, res){
+	var id = req.params.id;
+	db.serviceClients.remove({_id : mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+
+});
 
 app.listen(port, ipaddress);
