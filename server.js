7e2c9258aff03efa1c1cae6cfceb2877b3fa5b5c
GANG LIU <liu.ga@husky.neu.edu>
2014-10-09 12:01:37 -0700

    ud

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
-app.get("/hello", function (req, res){
-	res.json('hello world');
+app.get("/env", function (req, res){
+	res.json(process.env);
 });
 // //listen quest or url
 // app.get("/serviceClients", function(req, res){
